2cb7debcd12e5613547df2dc0109da7039ca1bd3
Erik Lager <65851326+warik21@users.noreply.github.com>
2023-12-17 15:09:32 +0200

    Map now matches real world coordinates

--- eriktron/src/main.py
+++ eriktron/src/main.py
@@ -10,48 +10,28 @@ import cv2
 import math
 
 
-def geo_to_pixel(lon, lat, geotransform):
-    """Convert from geographic coordinates to pixel coordinates."""
-    x_pixel = int((lon - geotransform[0]) / geotransform[1])
-    y_pixel = int((lat - geotransform[3]) / geotransform[5])
-    return x_pixel, y_pixel
-
-
 # Create a blank image
-image = cv2.imread('C:/Users/eriki/OneDrive/Documents/all_folder/other_projects/eriktron/eriktron/src/eilatall_tp.tif',
-                   cv2.IMREAD_UNCHANGED)
+im_path = r'C:/Users/eriki/OneDrive/Documents/all_folder/other_projects/eriktron/eriktron/src/eilat_new.tif'
+image = cv2.imread(im_path, cv2.IMREAD_UNCHANGED)
 # image = cv2.resize(image, (500, 500))
 
 # Create an instance of Observatory
 camera1_path = 'C:/Users/eriki/OneDrive/Documents/all_folder/other_projects/eriktron/eriktron/src/Observatories/Camera1.txt'
-camera1 = Observatory.load_from_file(camera1_path)
-# camera1 = Observatory(**camera_data)
-# Use attributes from camera1
-# Assuming camera1.fov_horizontal is in degrees. If it's in radians, convert it to degrees
-start_angle = 0
-end_angle = start_angle + camera1.fov_horizontal
+camera2_path = 'C:/Users/eriki/OneDrive/Documents/all_folder/other_projects/eriktron/eriktron/src/Observatories/Camera2.txt'
+#TODO: use glob for this, looking at all the files in Observatories
+camera_paths = [camera1_path, camera2_path]
 
-color = camera1.color  # Assuming color is in BGR format
+for camera_path in camera_paths:
+    camera = Observatory.load_from_file(camera_path)
+    # Create a mask for the observatory
+    mask = camera.draw_mask(image.shape, im_path)
 
-# Other parameters
-thickness = -1  # Fill the shape
-center_coordinates = (250, 250)
+    # Blend the original image and the mask
+    image = cv2.addWeighted(image, 1, mask, 0.5, 0)
 
-# Create a mask and draw the slice of pie
-mask_maximal = np.zeros_like(image)
-mask_minimal = np.zeros_like(image)
-cv2.ellipse(mask_maximal, center_coordinates, (camera1.distance_maximal, camera1.distance_maximal), 0, start_angle,
-            end_angle, color, thickness)
-cv2.ellipse(mask_minimal, center_coordinates, (camera1.distance_minimal, camera1.distance_minimal), 0, start_angle,
-            end_angle, color, thickness)
-
-mask = cv2.subtract(mask_maximal, mask_minimal)
-
-# Blend the original image and the mask
-result = cv2.addWeighted(image, 1, mask, 0.5, 0)
-
+result_ratio = image.shape[0] / image.shape[1]
 # Display the result
-print(image.shape)
-cv2.imshow('Slice of Pie', result)
+result_resized = cv2.resize(image, (500, int(500 * result_ratio)))
+cv2.imshow('Slice of Pie', result_resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- eriktron/src/utils/observatory.py
+++ eriktron/src/utils/observatory.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import rasterio
+import cv2
 from shapely.geometry import Point, MultiPolygon, Polygon
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
@@ -12,12 +13,13 @@ class Observatory:
     It will also include the minimal and maximal range that it can see, and the color.
     """
 
-    def __init__(self, latitude, longitude, distance_minimal, distance_maximal, fov_horizontal, fov_vertical, height,
-                 color):
+    def __init__(self, latitude, longitude, distance_minimal, distance_maximal, start_angle, fov_horizontal, 
+                 fov_vertical, height, color):
         self.latitude = latitude
         self.longitude = longitude
         self.distance_minimal = distance_minimal
         self.distance_maximal = distance_maximal
+        self.start_angle = start_angle
         self.fov_horizontal = fov_horizontal
         self.fov_vertical = fov_vertical
         self.height = height
@@ -89,9 +91,51 @@ class Observatory:
             longitude = float(data['longitude'])
             distance_minimal = int(data['distance_minimal'])
             distance_maximal = int(data['distance_maximal'])
+            start_angle = int(data['start_angle'])
             fov_horizontal = int(data['fov_horizontal'])
             fov_vertical = int(data['fov_vertical'])
             height = int(data['height_value'])
             color = tuple(map(int, data['color_value'][1:-1].split(',')))  # Parses "(255,0,0)" to (255, 0, 0)
 
-            return cls(latitude, longitude, distance_minimal, distance_maximal, fov_horizontal, fov_vertical, height, color)
+            return cls(latitude, longitude, distance_minimal, distance_maximal, start_angle, fov_horizontal, 
+                       fov_vertical, height, color)
+    
+    def get_pixel_location(self, geotiff_path):
+        """
+    This function gets the pixel location of the observatory in the image.
+    :param geotiff_path: The path of the geotiff file.
+    :return: The pixel location of the observatory in the image.
+    """
+        #TODO: Check whether this actually gets only the metadata and not the whole image
+        with rasterio.open(geotiff_path, 'r') as src:
+            # Transform the observatory's geographic coordinates to the image's coordinate system
+            x, y = src.index(self.latitude, self.longitude)
+
+
+        return x, y
+
+    def draw_mask(self, image_shape, geotiff_path):
+        """
+        This function draws the mask of the observatory on the image.
+        :param geotiff_path: The path of the geotiff file.
+        :return: The mask of the observatory.
+        """
+        mask_maximal: np.ndarray = np.zeros(image_shape)
+        mask_minimal: np.ndarray = np.zeros(image_shape)
+
+        center = self.get_pixel_location(geotiff_path)
+        start_angle = self.start_angle
+        end_angle = start_angle + self.fov_horizontal
+        thickness = -1
+
+
+        cv2.ellipse(mask_maximal, center, (self.distance_maximal, self.distance_maximal), 0,
+                    startAngle=start_angle, endAngle=end_angle, color=self.color, thickness=thickness)
+        cv2.ellipse(mask_minimal, center, (self.distance_minimal, self.distance_minimal), 0,
+                    startAngle=start_angle, endAngle=end_angle, color=self.color, thickness=thickness)
+        
+        mask = cv2.subtract(mask_maximal, mask_minimal)
+        mask = mask.astype('uint8')
+
+
+        return mask
